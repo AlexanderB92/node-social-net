f5ab2204091d722592bef6ee2047470352e379dc
AlexanderB92 <a.m.brandstrup@gmail.com>
2018-07-25 13:02:37 +0200

    added passport.js strategy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require('passport');
 
 //Routes
 const users = require("./routes/api/users");
@@ -26,9 +27,13 @@ mongoose
   .then(() => console.log("Mongoose connected to cloud-db"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
 
 //Map routes
 app.use("/api/users", users);
